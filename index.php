15bff9e180a66d4ea5afb298ef115aba59af10f1
Alejandro Naranjo <133051897+alex-stampede@users.noreply.github.com>
2026-03-21 14:57:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -55,6 +55,9 @@ try {
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="/assets/css/app.css">
+  <link rel="stylesheet" href="/assets/css/theme.css">
+<link rel="stylesheet" href="/assets/css/base.css">
+<link rel="stylesheet" href="/assets/css/components.css">
 </head>
 <body>
   <main class="shell">
